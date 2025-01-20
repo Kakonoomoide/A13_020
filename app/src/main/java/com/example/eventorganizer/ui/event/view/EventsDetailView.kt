a532a9593bae7c2af29b233d6d74447602b8ba5e
Kakonoomoide <rizkiramada128@gmail.com>
2025-01-20 23:42:33 +0700

    ADD - tampilan DetailViewScreen untuk menampilkan detail event. Termasuk tombol untuk mengedit event dan refresh data.

--- app/src/main/java/com/example/eventorganizer/ui/event/view/EventsDetailView.kt
+++ app/src/main/java/com/example/eventorganizer/ui/event/view/EventsDetailView.kt
@@ -1,5 +1,35 @@
 package com.example.eventorganizer.ui.event.view
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.eventorganizer.model.Events
+import com.example.eventorganizer.ui.PenyediaViewModel
+import com.example.eventorganizer.ui.costumwidget.CoustumeTopAppBar
+import com.example.eventorganizer.ui.event.viewmodel.DetailUiState
+import com.example.eventorganizer.ui.event.viewmodel.DetailViewModel
 import com.example.eventorganizer.ui.navigation.DestinasiNavigasi
 
 object DestinasiDetail: DestinasiNavigasi {
@@ -7,4 +37,123 @@ object DestinasiDetail: DestinasiNavigasi {
     override val titleRes = "Detail Event"
     const val IdEvent = "idEvent"
     val routesWithArg = "$route/{$IdEvent}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EventsDetailView(
+    navigateBack: () -> Unit,
+    navigateToItemUpdate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            CoustumeTopAppBar(
+                title = DestinasiDetail.titleRes,
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                onRefresh = {
+                    viewModel.getEventbyIdEvent()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemUpdate,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Event"
+                )
+            }
+        }
+    ) { innerPadding ->
+        DetailStatus(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = viewModel.mahasiswaDetailState,
+            retryAction = { viewModel.getEventbyIdEvent() }
+        )
+    }
+}
+
+@Composable
+fun DetailStatus(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState
+) {
+    when (detailUiState) {
+        is DetailUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+        is DetailUiState.Success -> {
+            if (detailUiState.events.idEvent.toString().isEmpty()) {
+                Box(
+                    modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center
+                ) { Text("Data tidak ditemukan.") }
+            } else {
+                ItemDetailEvnt(
+                    events = detailUiState.events,
+                    modifier = modifier.fillMaxWidth()
+                )
+            }
+        }
+        is DetailUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun ItemDetailEvnt(
+    modifier: Modifier = Modifier,
+    events: Events
+) {
+    Card(
+        modifier = modifier.padding(16.dp),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ComponentDetailEvnt(judul = "Id Event", isinya = events.idEvent.toString())
+            Spacer(modifier =Modifier.padding(5.dp))
+            ComponentDetailEvnt(judul = "Nama Event", isinya = events.namaEvent)
+            Spacer(modifier =Modifier.padding(5.dp))
+            ComponentDetailEvnt(judul = "Deskripsi Event", isinya = events.deskripsiEvent)
+            Spacer(modifier =Modifier.padding(5.dp))
+            ComponentDetailEvnt(judul = "Tanggal Event", isinya = events.tanggalEvent)
+            Spacer(modifier =Modifier.padding(5.dp))
+            ComponentDetailEvnt(judul = "Lokasi", isinya = events.lokasiEvent)
+            Spacer(modifier =Modifier.padding(5.dp))
+        }
+    }
+}
+
+@Composable
+fun ComponentDetailEvnt(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = judul,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
 }
