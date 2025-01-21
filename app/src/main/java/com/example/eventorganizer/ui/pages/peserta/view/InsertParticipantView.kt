fc1a52f86a4f12f36f44ca0863e72ab6b12c4394
Kakonoomoide <rizkiramada128@gmail.com>
2025-01-22 04:26:47 +0700

    ADD - tampilan dan form untuk memasukkan data peserta baru

--- app/src/main/java/com/example/eventorganizer/ui/pages/peserta/view/InsertParticipantView.kt
+++ app/src/main/java/com/example/eventorganizer/ui/pages/peserta/view/InsertParticipantView.kt
@@ -1,8 +1,168 @@
 package com.example.eventorganizer.ui.pages.peserta.view
 
+import android.app.DatePickerDialog
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.eventorganizer.ui.PenyediaViewModel
+import com.example.eventorganizer.ui.costumwidget.CoustumeTopAppBar
 import com.example.eventorganizer.ui.navigation.DestinasiNavigasi
+import com.example.eventorganizer.ui.pages.peserta.viewmodel.InsertParticipantViewModel
+import com.example.eventorganizer.ui.pages.peserta.viewmodel.ParticipantsInsertUiEvent
+import com.example.eventorganizer.ui.pages.peserta.viewmodel.ParticipantsInsertUiState
+import kotlinx.coroutines.launch
 
 object DestinasiEntryParticipants : DestinasiNavigasi {
     override val route = "participants_entry"
     override val titleRes = "Entry Participants"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ParticipantsInsertView(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertParticipantViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CoustumeTopAppBar(
+                title = DestinasiEntryParticipants.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onSiswaValueChange = viewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertMhs()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun EntryBody(
+    insertUiState: ParticipantsInsertUiState,
+    onSiswaValueChange: (ParticipantsInsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ){
+        FormInput(
+            insertUiEvent = insertUiState.participantsInsertUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    insertUiEvent: ParticipantsInsertUiEvent,
+    onValueChange: (ParticipantsInsertUiEvent) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+        // nama peserta
+        OutlinedTextField(
+            value = insertUiEvent.namaPeserta,
+            onValueChange = { onValueChange(insertUiEvent.copy(namaPeserta = it)) },
+            label = { Text("Nama Peserta/Pengguna") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        // email
+        OutlinedTextField(
+            value = insertUiEvent.email,
+            onValueChange = { onValueChange(insertUiEvent.copy(email = it)) },
+            label = { Text("Email Peserta/Pengguna") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Email
+            )
+        )
+
+        // no telpon
+        OutlinedTextField(
+            value = insertUiEvent.nomorTelepon,
+            onValueChange = { onValueChange(insertUiEvent.copy(nomorTelepon = it)) },
+            label = { Text("No. Telp. Peserta/Pengguna") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Phone
+            )
+        )
+
+        if(enabled) {
+            Text(
+                text = "Isi Semua Data!",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
 }
