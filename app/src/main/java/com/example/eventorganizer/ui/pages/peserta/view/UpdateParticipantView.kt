580911356c11de1e5221d7a9f825da0b607726d9
Kakonoomoide <rizkiramada128@gmail.com>
2025-01-22 04:30:32 +0700

    ADD - tampilan dan form untuk memperbarui data peserta

--- app/src/main/java/com/example/eventorganizer/ui/pages/peserta/view/UpdateParticipantView.kt
+++ app/src/main/java/com/example/eventorganizer/ui/pages/peserta/view/UpdateParticipantView.kt
@@ -1,10 +1,65 @@
 package com.example.eventorganizer.ui.pages.peserta.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.eventorganizer.ui.PenyediaViewModel
+import com.example.eventorganizer.ui.costumwidget.CoustumeTopAppBar
 import com.example.eventorganizer.ui.navigation.DestinasiNavigasi
+import com.example.eventorganizer.ui.pages.peserta.viewmodel.UpdateParticipantViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 object DestinasiUpdateParticipants: DestinasiNavigasi {
     override val route = "update"
     override val titleRes = "Update Participants"
     const val IdParticipants = "idPeserta"
     val routesWithArg = "$route/{$IdParticipants}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ParticipantsUpdateView(
+    onBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    onNavigate:()-> Unit,
+    viewModel: UpdateParticipantViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CoustumeTopAppBar(
+                title = DestinasiUpdateParticipants.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = onBack,
+            )
+        }
+    ){padding ->
+        EntryBody(
+            modifier = Modifier.padding(padding),
+            insertUiState = viewModel.participantsUpdateUiState,
+            onSiswaValueChange = viewModel::updateInsertPrcpState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updatePrcp()
+                    delay(600)
+                    withContext(Dispatchers.Main){
+                        onNavigate()
+                    }
+                }
+            }
+        )
+    }
 }
