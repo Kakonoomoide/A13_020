d07180f4f7c69870efd0e96ed6257abfb9f3ad7b
Kakonoomoide <rizkiramada128@gmail.com>
2025-01-21 20:38:41 +0700

    ADD - data class untuk respons tiket dan detail tiket dan PengelolaHalaman.kt yang nantinya akan di gunakan sebagai route

---
+++ app/src/main/java/com/example/eventorganizer/model/Tickets.kt
@@ -0,0 +1,48 @@
+package com.example.eventorganizer.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AllTicketsResponse (
+    val status: Boolean,
+    val message: String,
+    val data: List<Tickets>
+)
+
+@Serializable
+data class TicketsDetailResponse (
+    val status: Boolean,
+    val message: String,
+    val data: Tickets
+)
+
+@Serializable
+data class Tickets (
+    @SerialName("id_tiket")
+    val idTiket: Int,
+
+    @SerialName("id_event")
+    val idEvent: Int,
+
+    @SerialName("id_pengguna")
+    val idPengguna: Int,
+
+    @SerialName("Kapasitas_tiket")
+    val kapasitasTiket: Int,
+
+    @SerialName("harga_tiket")
+    val hargaTiket: Int,
+
+    @SerialName("nama_event")
+    val namaEvent: String,
+
+    @SerialName("tanggal_event")
+    val tanggalEvent: String,
+
+    @SerialName("lokasi_event")
+    val lokasiEvent: String,
+
+    @SerialName("nama_peserta")
+    val namaPeserta: String
+)
---
+++ app/src/main/java/com/example/eventorganizer/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,4 @@
+package com.example.eventorganizer.ui.navigation
+
+class PengelolaHalaman {
+}
