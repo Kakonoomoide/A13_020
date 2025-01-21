666fd203d8af41a0277d71c201a80710bf90339a
Kakonoomoide <rizkiramada128@gmail.com>
2025-01-22 03:47:41 +0700

    ADD - ViewModel dan state untuk insert peserta baru

---
+++ app/src/main/java/com/example/eventorganizer/ui/pages/peserta/viewmodel/InsertParticipantViewModel.kt
@@ -0,0 +1,60 @@
+package com.example.eventorganizer.ui.pages.peserta.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.eventorganizer.model.Participant
+import com.example.eventorganizer.repository.ParticipantsRepository
+import kotlinx.coroutines.launch
+
+class EventsInsertViewModel(
+    private val prcp: ParticipantsRepository
+): ViewModel() {
+    var uiState by mutableStateOf(ParticipantsInsertUiState())
+        private set
+
+    fun updateInsertMhsState(participantsInsertUiEvent: ParticipantsInsertUiEvent) {
+        uiState = ParticipantsInsertUiState(participantsInsertUiEvent = participantsInsertUiEvent)
+    }
+
+    suspend fun insertMhs() {
+        viewModelScope.launch {
+            try {
+                prcp.insertParticipants(uiState.participantsInsertUiEvent.toPrcp())
+            }catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+data class ParticipantsInsertUiState(
+    val participantsInsertUiEvent: ParticipantsInsertUiEvent = ParticipantsInsertUiEvent()
+)
+
+data class ParticipantsInsertUiEvent(
+    val idPeserta: Int = 0,
+    val namaPeserta: String = "",
+    val email: String = "",
+    val nomorTelepon: String = ""
+)
+
+fun ParticipantsInsertUiEvent.toPrcp(): Participant = Participant(
+    idPeserta = idPeserta,
+    namaPeserta = namaPeserta,
+    email = email, 
+    nomorTelepon = nomorTelepon
+)
+
+fun Participant.toUiStatePrcp(): ParticipantsInsertUiState = ParticipantsInsertUiState(
+    participantsInsertUiEvent = toPrcpInsertUiEvent()
+)
+
+fun Participant.toPrcpInsertUiEvent(): ParticipantsInsertUiEvent = ParticipantsInsertUiEvent(
+    idPeserta = idPeserta,
+    namaPeserta = namaPeserta,
+    email = email,
+    nomorTelepon = nomorTelepon
+)
